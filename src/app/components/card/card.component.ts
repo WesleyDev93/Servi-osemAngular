e6ac37bcb65157657c8cc15354c0da002cfeabe9
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-21 11:32:34 -0300

    Criando service, e a pasta environments no nossa estrutura

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
+// vamos moldar os dados pela classe
 export class CardComponent {
+
+
+  // variaveis de dados
+  name: string = "Bulbasar";
+  tipos: string[] = ['Fire', 'Rock'];
+
+
+constructor () {}
 
 }
---
+++ src/app/services/pokemon-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+
+// injectable é como uma Injeção de dados , podemos utilizar ele dentro do nosso constructor
+// podemos injetar ele diretamento em um modulo , colocar a classe em PROVIDERS:[]
+@Injectable({
+  // aqui ele diz onde ele esta sendo injetado que seria no root inicialmente
+  // quer dizer que todos os nosso componentes veem ele e tem acesso
+  providedIn: 'root'
+})
+
+// classe normal que estamos habituados a ver
+export class PokemonServiceService {
+
+  constructor() { }
+}
